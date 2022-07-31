5c19cd23f832931d6eae136cae86edd9b885f771
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-31 21:21:46 +0000

    Add getOrderById function

--- database.js
+++ database.js
@@ -33,3 +33,33 @@ const getAllOrders = function(limit = 6) {
 
 exports.getAllOrders = getAllOrders;
 
+/**
+ * Get one order from db with it's id
+ * @param {string} id The id of the order.
+ * @return {Promise<{}>} A promise to the order.
+ */
+
+const getOrderWithId = function(id) {
+  const queryString = `
+    SELECT orders.*,
+      users.name as user_name,
+      users.phone as user_phone_number,
+      admins.name as admin_name,
+      admin.phone as admin_phone_number,
+      json_agg(json_build_object('item_name', items.name, 'quantity', ordered_items.quantity, 'price', (price * quantity), 'photo_url', photo_url)) as coffee_items,
+      SUM(price * quantity) as total
+    FROM orders
+    JOIN admins ON admins.id = admin_id
+    JOIN users ON users.id = user_id
+    JOIN ordered_items ON order_id = orders.id
+    JOIN items ON items.id = item_id
+    WHERE orders.id = $1
+    GROUP BY orders.id, users.name, users.phone, admins.name, admins.phone;
+  `;
+
+  return pool
+    .query(queryString, [id])
+    .then(result => result.rows[0])
+    .catch(err => err.message)
+};
+exports.getOrderWithId = getOrderWithId;
