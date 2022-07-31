ecb2c336f32e463401e7590e6bfdac2d70da9b43
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-31 23:02:10 +0000

    create addItems function to create html divs for each item

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,49 +5,37 @@ $(() => {
     return;
   });
 
-  // // Toggle hot menu
+  // Toggle hot menu
   $('.hot').on('click', () => {
-    if ($('.hot-menu').css('display') == 'none') {
-      $('#menus').slideToggle(0);
-      $('header').slideToggle(0);
-      $('.hot-menu').css('display', 'flex')
+    if ($('.menu').css('display') == 'none') {
+      addItems(testDB);
+      $('#menus').css('display', 'none');
+      $('header').css('display', 'none');
+      $('.menu').css('display', 'flex')
       return;
     }
-
-    $('#menus').slideToggle(0);
-    $('header').slideToggle(0);
-    $('.hot-menu').css('display', 'none')
-    return;
   });
 
   // Toggle cold menu
   $('.cold').on('click', () => {
     if ($('.cold-menu').css('display') == 'none') {
-      $('#menus').slideToggle(0);
-      $('header').slideToggle(0);
+      $('.menu').css('display', 'none');
+      $('#menus').css('display', 'none');
+      $('header').css('display', 'none');
       $('.cold-menu').css('display', 'flex')
       return;
     }
-
-    $('#menus').slideToggle(0);
-    $('header').slideToggle(0);
-    $('.cold-menu').css('display', 'none')
-    return;
   });
 
   // Toggle baked-goods menu
   $('.baked').on('click', () => {
     if ($('.baked-menu').css('display') == 'none') {
-      $('#menus').slideToggle(0);
-      $('header').slideToggle(0);
+      $('.menu').css('display', 'none');
+      $('#menus').css('display', 'none');
+      $('header').css('display', 'none');
       $('.baked-menu').css('display', 'flex')
       return;
     }
-
-    $('#menus').slideToggle(0);
-    $('header').slideToggle(0);
-    $('.baked-menu').css('display', 'none')
-    return;
   });
 
   // Toggle home menu
--- public/scripts/items_menu.js
+++ public/scripts/items_menu.js
@@ -1,27 +1,61 @@
-$(() => {
-
-  const $itemsMenu = $(`main`);
-  window.$itemsMenu = $itemsMenu;
-
-  window.itemsMenu = {};
-
-  const addItem = item => {
-    $itemsMenu.append(item);
-  }
-  const clearItems = () => {
-    $itemsMenu.empty();
+// Creates an item div
+const createItem = (item) => {
+  if (item.type === 'baked') {
+    return `
+      <div class='baked-item'>
+        <img src="${item.photo_url}">
+        <span>${item.name}</span>
+      </div>
+      `
   }
 
-  window.itemsMenu.clearItems = clearItems;
+  return `
+    <div class='drink-item'>
+      <img src="${item.photo_url}">
+      <span>${item.name}</span>
+    </div>
+    `
+}
 
-  const addItems = (properties, isReservation = false) => {
-    clearListings();
-    for (const itemId in items) {
-      const item = items[itemId];
-      const newItem = item.createItem(item, type);
-      addItem(newItem);
-    }
+// Appends item
+const addItem = item => {
+  $('.menu').append(item);
+}
+
+// Clears items
+const clearItems = () => {
+  $('.menu').empty();
+}
+
+// Clears then adds all menu items
+const addItems = (items) => {
+  clearItems();
+  for (const itemId in items) {
+    const item = items[itemId];
+    const newItem = createItem(item);
+    addItem(newItem);
   }
-  window.itemsMenu.addItem = addItems;
+}
 
-});
+// Test Data
+let testDB = [{
+  name: 'coffee',
+  type: 'drink',
+  photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG",
+},
+{
+  name: 'espresso',
+  type: 'drink',
+  photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeIUUNTss4cG9_z36HD1p8zydMeqyaWXmqdA&usqp=CAU"
+},
+{
+  name: 'donut',
+  type: 'baked',
+  photo_url: "https://images.unsplash.com/photo-1626094309830-abbb0c99da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80"
+},
+{
+  name: 'donut',
+  type: 'baked',
+  photo_url: "https://images.unsplash.com/photo-1626094309830-abbb0c99da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80"
+}]
+
