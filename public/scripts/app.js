73875db54a4baf9fd5a2754539585fc7c514d4e1
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-31 02:23:16 +0000

    attempt to add items to hot-menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,30 @@
-// Toggle cart
 $(() => {
+  // Toggle cart
   $('#cart-icon').on('click', () => {
     $('#cart-menu').slideToggle(0);
     return;
   });
 
-  $('#menu div').on('click', () => {
-    $('#menu').slideToggle(0);
+  // Toggle hot menu
+  $('.hot').on('click', () => {
+    if ($('.menu').css('display') == 'none') {
+      $('#menus').slideToggle(0);
+      $('header').slideToggle(0);
+      $('.hot-menu').css('display', 'flex')
+      return;
+    }
+    $('#menus').slideToggle(0);
     $('header').slideToggle(0);
+    $('.hot-menu').css('display', 'none')
     return;
   });
 
-  $('#home').on('click', () => {
-    if ($('#menu').css('display') == 'none') {
-      $('#menu').slideToggle(0);
-      $('header').slideToggle(0);
-      return;
-    }
-  })
+  // // Toggle home menu
+  // $('#home').on('click', () => {
+  //   if ($('#menu').css('display') == 'none') {
+  //     $('#menu').slideToggle(0);
+  //     $('header').slideToggle(0);
+  //     return;
+  //   }
+  // })
 });
