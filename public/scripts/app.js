6db03e50d8dddd3431fa4fc82d0fa4e684212df7
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-31 17:58:08 +0000

    comment out code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,9 @@ $(() => {
     return;
   });
 
-  // Toggle hot menu
+  // // Toggle hot menu
   $('.hot').on('click', () => {
-    if ($('.menu').css('display') == 'none') {
+    if ($('.hot-menu').css('display') == 'none') {
       $('#menus').slideToggle(0);
       $('header').slideToggle(0);
       $('.hot-menu').css('display', 'flex')
@@ -20,35 +20,36 @@ $(() => {
     return;
   });
 
-  // Toggle cold menu
-  $('.cold').on('click', () => {
-    if ($('.menu').css('display') == 'none') {
-      $('#menus').slideToggle(0);
-      $('header').slideToggle(0);
-      $('.cold-menu').css('display', 'flex')
-      return;
-    }
 
-    $('#menus').slideToggle(0);
-    $('header').slideToggle(0);
-    $('.cold-menu').css('display', 'none')
-    return;
-  });
+  // // Toggle cold menu
+  // $('.cold').on('click', () => {
+  //   if ($('.cold-menu').css('display') == 'none') {
+  //     $('#menus').slideToggle(0);
+  //     $('header').slideToggle(0);
+  //     $('.cold-menu').css('display', 'flex')
+  //     return;
+  //   }
 
-  // Toggle baked-goods menu
-  $('.baked').on('click', () => {
-    if ($('.menu').css('display') == 'none') {
-      $('#menus').slideToggle(0);
-      $('header').slideToggle(0);
-      $('.baked-menu').css('display', 'flex')
-      return;
-    }
+  //   $('#menus').slideToggle(0);
+  //   $('header').slideToggle(0);
+  //   $('.cold-menu').css('display', 'none')
+  //   return;
+  // });
 
-    $('#menus').slideToggle(0);
-    $('header').slideToggle(0);
-    $('.baked-menu').css('display', 'none')
-    return;
-  });
+  // // // Toggle baked-goods menu
+  // $('.baked').on('click', () => {
+  //   if ($('.baked-menu').css('display') == 'none') {
+  //     $('#menus').slideToggle(0);
+  //     $('header').slideToggle(0);
+  //     $('.baked-menu').css('display', 'flex')
+  //     return;
+  //   }
+
+  //   $('#menus').slideToggle(0);
+  //   $('header').slideToggle(0);
+  //   $('.baked-menu').css('display', 'none')
+  //   return;
+  // });
 
   // Toggle home menu
   $('#home').on('click', () => {
