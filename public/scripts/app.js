7da6bc1e2ce775708c02e500d719b2750a34388f
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-31 06:30:47 +0000

    Initial commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
 // Client facing scripts here
+
+$(document).ready(function () {
+    $(".btn1").click(function () {
+        $("#hidediv").toggle();
+        $("#showingdiv").toggle();
+    });
+
+});
+
