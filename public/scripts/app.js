2231f8f868af186d69d5bf440daae34960339fe0
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-31 03:46:00 +0000

    add menu toggle to all menus

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,18 +13,50 @@ $(() => {
       $('.hot-menu').css('display', 'flex')
       return;
     }
+
     $('#menus').slideToggle(0);
     $('header').slideToggle(0);
     $('.hot-menu').css('display', 'none')
     return;
   });
 
-  // // Toggle home menu
-  // $('#home').on('click', () => {
-  //   if ($('#menu').css('display') == 'none') {
-  //     $('#menu').slideToggle(0);
-  //     $('header').slideToggle(0);
-  //     return;
-  //   }
-  // })
+  // Toggle cold menu
+  $('.cold').on('click', () => {
+    if ($('.menu').css('display') == 'none') {
+      $('#menus').slideToggle(0);
+      $('header').slideToggle(0);
+      $('.cold-menu').css('display', 'flex')
+      return;
+    }
+
+    $('#menus').slideToggle(0);
+    $('header').slideToggle(0);
+    $('.cold-menu').css('display', 'none')
+    return;
+  });
+
+  // Toggle baked-goods menu
+  $('.baked').on('click', () => {
+    if ($('.menu').css('display') == 'none') {
+      $('#menus').slideToggle(0);
+      $('header').slideToggle(0);
+      $('.baked-menu').css('display', 'flex')
+      return;
+    }
+
+    $('#menus').slideToggle(0);
+    $('header').slideToggle(0);
+    $('.baked-menu').css('display', 'none')
+    return;
+  });
+
+  // Toggle home menu
+  $('#home').on('click', () => {
+    if ($('#menus').css('display') == 'none') {
+      $('.menu').css('display', 'none')
+      $('#menus').slideToggle(0);
+      $('header').slideToggle(0);
+      return;
+    }
+  })
 });
