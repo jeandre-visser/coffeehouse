e9d879353a7e23c15efc1e96f08ad516b24889d5
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-08-02 15:59:37 +0000

    Grabbing data about user and order from getAllOrders query

--- routes/summary-routes.js
+++ routes/summary-routes.js
@@ -3,30 +3,38 @@ const router  = express.Router();
 
 const databaseFunctions = require('../database');
 
-
-// const textMessage = require('../helper_functions/textMessage');
-// console.log("textMessage", textMessage);
-
 module.exports = (db) => {
 
   // Adding summary page
   router.get("/:id", (req, res) => {
-    console.log("check1");
     databaseFunctions.getAllOrders().then(data => {
-      console.log("data", data)
-      console.log("data1", data[0].phone);
+      const name = data[0].name;
+      const phone = data[0].phone;
+      const timeOfOrder = data[0].order_timestamp;
+      const coffeeItems = data[0].coffee_items;
+      const items = coffeeItems[0].item_name;
+      const quantity = coffeeItems[0].quantity;
 
-    })
+      console.log("name", name);
+      console.log("phone", phone);
+      console.log("time", timeOfOrder);
+      console.log(`Order: ${quantity} cups of ${items} ordered at ${timeOfOrder}.`)
+
+    });
+
+    // Need to grab name outside of the function
+    // And use it for TemplateVars to show in
+    /*
+    const templateVars = {
+      name: data[0].name,
+      phone: data[0].phone
+    };
+
+    etc ...
+    */
 
     res.render("summary");
   });
-
-
-
-
-
-
-
 
   return router;
 };
