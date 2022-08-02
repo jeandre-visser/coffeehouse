154ef888e50741dee530f37dadf0c8c1593b7a2f
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-02 23:05:53 +0000

    Add new menu file

---
+++ routes/menus.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { getItemsByCategory } = require('../database');
+const router  = express.Router();
+
+
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM items;`)
+      .then(data => {
+        const items = data.rows;
+        res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
