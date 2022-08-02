229a47c5066fc29f0d51afad4d666c7a85c4a394
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-02 08:34:03 +0000

    change css and connect options-menu to item

--- public/scripts/options_menu.js
+++ public/scripts/options_menu.js
@@ -1,11 +1,11 @@
-const optionsMenu = (itemId) => {
+const optionsMenu = (items, itemId) => {
   return ` <section class="options-menu">
   <div class="item-summary">
-    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG'>
+    <img src='${items[itemId].photo_url}'>
     <div class='summary'>
-      <h1>Coffee</h1>
-      <div>A hot cup of bean water</div>
-      <div>$100</div>
+      <h1>${items[itemId].name}</h1>
+      <div>${items[itemId].description}</div>
+      <div>$${items[itemId].price}</div>
     </div>
   </div>
   <form class="order">
@@ -14,7 +14,7 @@ const optionsMenu = (itemId) => {
       <input type="number" name="quantity" min="1" max="99">
     </div>
     <div>
-      <button type="submit">Add to Order: $100</button>
+      <button type="submit">Add to Order: ${items[itemId].price}</button>
     </div>
   </form>
 </section>
