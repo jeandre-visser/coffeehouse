d6b0e64b91ace1e0d7ee436230453fe405c8c0ca
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-03 00:08:25 +0000

    still trying to merge...

--- routes/menus.js
+++ routes/menus.js
@@ -2,24 +2,6 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-<<<<<<< HEAD
-  // Menu page for either hot drinks, cold drinks, or baked goods
-  router.get('/items/:category', (req, res) => {
-    db.query(
-      (`SELECT * FROM items WHERE category = $1`, req.params.category)
-    )
-    .then(result => {
-      const items = result.rows;
-      res.json({ items });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    })
-  }
-)}
-=======
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM items;`)
       .then(data => {
@@ -34,4 +16,3 @@ module.exports = (db) => {
   });
   return router;
 };
->>>>>>> master
