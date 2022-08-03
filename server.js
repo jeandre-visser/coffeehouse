ab62cc9e607d4d89d42ec6c0d195838b9c644011
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-03 17:30:00 +0000

    Updated

--- server.js
+++ server.js
@@ -53,16 +53,10 @@ app.use("/menus", menusRoutes(db));
 app.use("/category/", itemRoutes(db));
 app.use("/cart/", cartRoutes(db));
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
-=======
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
 // Note: mount other resources here, using the same pattern above
 
-// Home pa-cart, cart(db);// Warning: avoid creating more routes in this file!
+// Home page
+// Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
@@ -72,4 +66,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-
