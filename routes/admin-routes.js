ca9c94526d5ca74c274ba19df836328667308721
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-04 04:30:52 +0000

    Temporarily commented out code

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const router  = express.Router();
+// const express = require('express');
+// const router  = express.Router();
 
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`
-      SELECT orders.id, orders.user_id, order_timestamp, ordered_items.item_id, ordered_items.quantity
-      FROM orders
-      JOIN ordered_items ON orders.id = ordered_items.order_id;`)
-      .then(data => {
-        const orders = data.rows;
-        res.json({orders});
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+// module.exports = (db, database) => {
+//   router.get("/", (req, res) => {
+//     db.query()
+//       .then(data => {
+//         const orders = data.rows;
+//         res.json({orders});
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
 
-  router.post('/', (req, res) => {
-    // console.log('req.body:',req.body)
-    // if(req) {
-    //   db.query(`INSERT items $1 RETURNING id`)
-    //   .then(()db.query(`INSERT orders $1 `)
-    //     })
-    // }
-    res.sendStatus(201);
-  })
-  return router;
-};
+//   router.post('/admin', (req, res) => {
+//     if(req.session.cart) {
+//       database.createUser(req.body.name, req.body.phone)
+//       // admin_id is hard coded as 1
+//         .then(user => database.createOrder(user.id, 1, new Date(), true, false))
+//         .then(order => database.addToOrderedItems(order.id, req.session.cart))
+//         .then(order => database.getOrderWithId(order.order_id))
+//         .then(order => {
+//           req.session.cart = null;
+//           textMessage(`Hello ${order.admin_name}, a new order has been placed!.`, `+1${order.admin_phone}`, `+1${order.user_phone}`)
+//           res.redirect(`/summary/${order.id}`)
+//         })
+//     }
+//   })
+//   return router;
+// };
