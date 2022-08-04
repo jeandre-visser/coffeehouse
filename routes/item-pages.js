44b6a08d142fcd8f73ae71282c3d9a38ce6a6265
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-04 01:15:56 +0000

    Fixed error

--- routes/item-pages.js
+++ routes/item-pages.js
@@ -1,26 +1,9 @@
 const express = require('express');
-<<<<<<< HEAD
-<<<<<<< HEAD
-const { getItemsByCategory } = require('../database');
-const router  = express.Router();
-
-
-
-module.exports = (db) => {
-  router.get("/:category", (req, res) => {
-    db.query(`SELECT * FROM items WHERE category = $1`, [req.params.category])
-=======
-=======
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
 const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/:category", (req, res) => {
     db.query(`SELECT * FROM items WHERE category = $1;`, [req.params.category])
-<<<<<<< HEAD
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
-=======
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
       .then(data => {
         const items = data.rows;
         res.json({ items });
@@ -31,12 +14,5 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
-=======
->>>>>>> 0b49830841d34ae5ae95dd757e53ac5c4480333b
   return router;
 };
