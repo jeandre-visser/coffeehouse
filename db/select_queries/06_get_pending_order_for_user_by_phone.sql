b2310e00b93184459d37f32d803880778371de64
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-05 00:50:26 +0000

    clean up files and add twilio

--- db/select_queries/06_get_pending_order_for_user_by_phone.sql
+++
@@ -1,8 +0,0 @@
-SELECT orders.id as order_id, users.phone
-FROM orders
-JOIN users ON orders.user_id = users.id
-JOIN ordered_items ON ordered_items.order_id = orders.id
-JOIN cup_sizes ON cup_sizes.id = ordered_items.size_id
-JOIN items ON items.id = ordered_items.item_id
-WHERE orders.order_pending= TRUE AND orders.order_ready = FALSE AND phone = '6398401087'
-GROUP BY users.phone, orders.id;
